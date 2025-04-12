6c84d969dc1acfd95d221b85c8bf820e6839410c
matsu <tmtmt2209@gmail.com>
2025-04-12 03:19:54 +0000

    chore: update

--- crates/relay/src/main.rs
+++ crates/relay/src/main.rs
@@ -16,30 +16,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = listener.into_stream();
 
     while let Ok(Some(_)) = stream.try_next().await {
-        match sqlx::query_as!(
-            Outbox,
-            r#"
-            select
-                id,
-                topic,
-                payload
-            from
-                outbox
-            where
-                processed_at is null
-            order by
-                created_at asc
-            for update skip locked          
-        "#
-        )
-        .fetch_all(&pool)
-        .await
-        {
-            Ok(list) => {
-                dbg!(&list);
-            }
-            Err(_) => continue,
-        }
+        let _ = process(&pool).await;
     }
 
     Ok(())
@@ -50,4 +27,47 @@ struct Outbox {
     id: i64,
     topic: String,
     payload: String,
+}
+
+async fn process(pool: &sqlx::Pool<sqlx::Postgres>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut tx = pool.begin().await?;
+
+    let list = sqlx::query_as!(
+        Outbox,
+        r#"
+        select
+            id,
+            topic,
+            payload
+        from
+            outbox
+        where
+            processed_at is null
+        order by
+            created_at asc
+        for update
+            skip locked
+    "#
+    )
+    .fetch_all(&mut *tx)
+    .await?;
+
+    for outbox in list {
+        println!("Processing outbox: {:?}", outbox);
+
+        sqlx::query!(
+            r#"
+            update outbox
+            set processed_at = now()
+            where id = $1
+        "#,
+            outbox.id
+        )
+        .execute(&mut *tx)
+        .await?;
+    }
+
+    tx.commit().await?;
+
+    Ok(())
 }
