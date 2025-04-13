ab29b408b71e8b7458573c38818b6452a20d3f46
matsu <tmtmt2209@gmail.com>
2025-04-13 09:13:45 +0000

    chore: update

--- crates/relay/src/main.rs
+++ crates/relay/src/main.rs
@@ -1,11 +1,21 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = std::env::var("DATABASE_URL")?;
+    let channel = {
+        let amqp_url = std::env::var("AMQP_URL")?;
+        let options = lapin::ConnectionProperties::default()
+            .with_executor(tokio_executor_trait::Tokio::current())
+            .with_reactor(tokio_reactor_trait::Tokio);
+        let conn = lapin::Connection::connect(&amqp_url, options).await?;
+        conn.create_channel().await?
+    };
 
-    let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(2)
-        .connect(&database_url)
-        .await?;
+    let pool = {
+        let database_url = std::env::var("DATABASE_URL")?;
+        sqlx::postgres::PgPoolOptions::new()
+            .max_connections(2)
+            .connect(&database_url)
+            .await?
+    };
 
     let mut listener = sqlx::postgres::PgListener::connect_with(&pool).await?;
 
@@ -13,7 +23,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let listener_stream = {
         let stream = listener.into_stream();
-        futures_util::StreamExt::map(stream, |r| r.map(|_|()))
+        futures_util::StreamExt::map(stream, |r| r.map(|_| ()))
     };
 
     let timer_stream = {
@@ -26,26 +36,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut combined_stream = tokio_stream::StreamExt::merge(listener_stream, timer_stream);
 
     while let Ok(Some(_)) = futures_util::TryStreamExt::try_next(&mut combined_stream).await {
-        let _ = process_outbox(&pool).await;
+        let _ = process_outbox(&pool, async |outbox| {
+            println!("Processing outbox message: {:?}", outbox);
+
+            channel
+                .basic_publish(
+                    "",
+                    &outbox.topic,
+                    lapin::options::BasicPublishOptions::default(),
+                    outbox.payload.as_bytes(),
+                    lapin::BasicProperties::default(),
+                )
+                .await?;
+
+            Ok(())
+        })
+        .await;
     }
 
     Ok(())
 }
 
 #[derive(Debug)]
-struct Outbox {
+struct OutboxMessage {
     id: i64,
     topic: String,
     payload: String,
 }
 
 async fn process_outbox(
-    pool: &sqlx::Pool<sqlx::Postgres>
+    pool: &sqlx::Pool<sqlx::Postgres>,
+    handler: impl AsyncFn(&OutboxMessage) -> Result<(), Box<dyn std::error::Error>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut tx = pool.begin().await?;
 
     let list = sqlx::query_as!(
-        Outbox,
+        OutboxMessage,
         r#"
         select
             id,
@@ -65,7 +91,7 @@ async fn process_outbox(
     .await?;
 
     for outbox in list {
-        println!("Processing outbox: {:?}", outbox);
+        handler(&outbox).await?;
 
         sqlx::query!(
             r#"
