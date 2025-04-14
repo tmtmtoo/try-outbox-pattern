aa01d56770047c8ba3ed6b38992d902a91ce545e
tmtmtoo <tmtmt2209@gmail.com>
2025-04-14 11:11:07 +0900

    chore: update

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -2,34 +2,34 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let database_url = std::env::var("DATABASE_URL")?;
 
-    let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(1)
-        .connect(&database_url)
-        .await?;
-
-    let mut tx = pool.begin().await?;
+    use sqlx::Connection;
+    let mut conn = sqlx::postgres::PgConnection::connect(&database_url).await?;
 
     let (post, event) = post("Hello, world!", "This is my post.");
 
-    sqlx::query!(
-        "insert into post (id, title, content) values ($1, $2, $3)",
-        post.id,
-        post.title,
-        post.content
-    )
-    .execute(&mut *tx)
-    .await?;
+    {
+        let mut tx = conn.begin().await?;
 
-    sqlx::query!(
-        "insert into outbox (topic, payload, created_at) values ($1, $2, $3)",
-        event.topic(),
-        event.payload(),
-        event.created_at()
-    )
-    .execute(&mut *tx)
-    .await?;
+        sqlx::query!(
+            "insert into post (id, title, content) values ($1, $2, $3)",
+            post.id,
+            post.title,
+            post.content
+        )
+        .execute(&mut *tx)
+        .await?;
 
-    tx.commit().await?;
+        sqlx::query!(
+            "insert into outbox (topic, payload, created_at) values ($1, $2, $3)",
+            event.topic(),
+            event.payload(),
+            event.created_at()
+        )
+        .execute(&mut *tx)
+        .await?;
+
+        tx.commit().await?;
+    }
 
     Ok(())
 }
