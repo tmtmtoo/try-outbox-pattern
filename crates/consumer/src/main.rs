eb1d7af587eee690bb8513de792bc61b64fbcc1b
matsu <tmtmt2209@gmail.com>
2025-04-19 12:17:13 +0000

    chore: update

--- crates/consumer/src/main.rs
+++ crates/consumer/src/main.rs
@@ -1,12 +1,22 @@
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    amqp_url: String,
+    consumer_duration_secs: u32,
+    consumer_failure_rate: f64,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use futures_util::stream::TryStreamExt;
+
+    let config = envy::from_env::<Config>()?;
+
     let amqp_channel = {
-        let amqp_url = std::env::var("AMQP_URL")?;
         let options = lapin::ConnectionProperties::default()
             .with_executor(tokio_executor_trait::Tokio::current())
             .with_reactor(tokio_reactor_trait::Tokio);
-        let conn = lapin::Connection::connect(&amqp_url, options).await?;
-        conn.create_channel().await?
+        let conn = lapin::Connection::connect(&config.amqp_url, options).await?;
+        conn.create_channel().await.map(std::sync::Arc::new)?
     };
 
     let mut stream = amqp_channel
@@ -21,64 +31,80 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .await?;
 
-    use futures_util::stream::TryStreamExt;
     while let Some(message) = stream.try_next().await? {
-        let data = String::from_utf8_lossy(&message.data);
-        let posted = serde_json::from_str::<event::Posted>(&data)?;
-        println!("Received message: {posted:?}");
-
-        let timeout_duration = tokio::time::Duration::from_secs(5);
-        let result = tokio::time::timeout(
-            timeout_duration,
-            process_may_error(rand::random_range(0..7)),
-        )
-        .await;
-
-        match result {
-            Ok(Ok(_)) => {
-                println!("Processing succeeded");
-                amqp_channel
-                    .basic_ack(
-                        message.delivery_tag,
-                        lapin::options::BasicAckOptions::default(),
-                    )
-                    .await?;
-            }
-            Ok(Err(_)) => {
-                println!("Processing failed, requeuing");
-                amqp_channel
-                    .basic_nack(
-                        message.delivery_tag,
-                        lapin::options::BasicNackOptions {
-                            requeue: true,
-                            ..Default::default()
-                        },
-                    )
-                    .await?;
-            }
-            Err(_) => {
-                println!("Processing timed out, requeuing");
-                amqp_channel
-                    .basic_nack(
-                        message.delivery_tag,
-                        lapin::options::BasicNackOptions {
-                            requeue: true,
-                            ..Default::default()
-                        },
-                    )
-                    .await?;
-            }
-        }
+        let amqp_channel = amqp_channel.clone();
+        let task = async move {
+            process_message(amqp_channel.clone(), message, async move |event| {
+                println!("Received event: {:?}", event);
+                tokio::time::sleep(tokio::time::Duration::from_secs(
+                    config.consumer_duration_secs.into(),
+                ))
+                .await;
+                random_error(config.consumer_failure_rate)?;
+                Ok(())
+            })
+            .await
+        };
+        tokio::spawn(task);
     }
 
     Ok(())
 }
 
-async fn process_may_error(duration_sec: u32) -> Result<(), ()> {
-    tokio::time::sleep(tokio::time::Duration::from_secs(duration_sec.into())).await;
-    if rand::random::<bool>() {
-        Ok(())
+async fn process_message(
+    amqp_channel: std::sync::Arc<lapin::Channel>,
+    message: lapin::message::Delivery,
+    handler: impl AsyncFn(
+        event::Posted,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>>,
+) {
+    let data = String::from_utf8_lossy(&message.data);
+
+    let event = match serde_json::from_str::<event::Posted>(&data) {
+        Ok(event) => event,
+        Err(_) => {
+            let _ = amqp_channel
+                .basic_nack(
+                    message.delivery_tag,
+                    lapin::options::BasicNackOptions {
+                        requeue: true,
+                        ..Default::default()
+                    },
+                )
+                .await;
+            return;
+        }
+    };
+
+    match handler(event).await {
+        Ok(_) => {
+            let _ = amqp_channel
+                .basic_ack(
+                    message.delivery_tag,
+                    lapin::options::BasicAckOptions::default(),
+                )
+                .await;
+        }
+        Err(_) => {
+            let _ = amqp_channel
+                .basic_nack(
+                    message.delivery_tag,
+                    lapin::options::BasicNackOptions {
+                        requeue: true,
+                        ..Default::default()
+                    },
+                )
+                .await;
+        }
+    }
+}
+
+fn random_error(
+    failure_rate: f64,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    if rand::random_range(0.0..=1.0) < failure_rate {
+        Err("Random error occurred".into())
     } else {
-        Err(())
+        Ok(())
     }
 }
