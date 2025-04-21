f6afeb5ca83bf95edc7d51abcb9a2f545936f7c8
tmtmtoo <tmtmt2209@gmail.com>
2025-04-21 10:11:53 +0900

    chore: update

--- migrations/20250411011524_post.down.sql
+++ migrations/20250411011524_post.down.sql
@@ -2,5 +2,6 @@
 
 drop trigger if exists outbox_notify_trigger on outbox;
 drop function if exists notify_outbox_event();
+drop index if exists idx_outbox_processed_created;
 drop table if exists outbox;
 drop table if exists post;
